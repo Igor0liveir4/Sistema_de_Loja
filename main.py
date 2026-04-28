b69c3b3de8961a0e3a0e9e5dab2105217ca6b297
Igor <igoroliveirapupo3@gmail.com>
2026-04-28 16:12:46 -0300

    add a rota principal e a tela do home ("/")

--- main.py
+++ main.py
@@ -3,12 +3,29 @@ from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from database import get_db
-from models import Curso, Aluno
+from models import Categoria, Produto
 
 #pip install jinja2 python-multipart
 
-app = FastAPI(title="Gestão escolar")
+app = FastAPI(title="Sistema de loja")
 templates = Jinja2Templates(directory="templates")
 
 #Rodar api
 #No terminal: python -m uvicorn main:app --reload
+
+@app.get("/")
+def home(request:Request):
+    return templates.TemplateResponse(
+        request,
+        "index.html",
+        {"request":request}
+    )
+
+#Rota para cadastrar produto
+@app.post("/produtos")
+def criar_produto(
+    nome: str = Form(...),
+    preco: float = Form(...),
+    estoque: int = Form(...)
+):
+    return {"nome": nome, "preco": preco, "estoque": estoque}
