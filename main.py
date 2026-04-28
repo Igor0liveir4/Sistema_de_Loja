4c5619d20a1dad72c4e870215fd021ef68f224db
Igor <igoroliveirapupo3@gmail.com>
2026-04-28 16:29:03 -0300

    ajustado alguns erros e adiconado a rota de cadastrar produtos ✔✔

--- main.py
+++ main.py
@@ -21,11 +21,35 @@ def home(request:Request):
         {"request":request}
     )
 
-#Rota para cadastrar produto
+@app.get("/produtos/cadastro")
+def exibir_cadastro(request: Request, db: Session = Depends(get_db)):
+    # Busca todas as categorias no banco
+    categorias = db.query(Categoria).all()
+    return templates.TemplateResponse(
+        request,
+        "cadastrar_produto.html",
+        {"request": request, "categorias": categorias}
+    )
+
 @app.post("/produtos")
 def criar_produto(
     nome: str = Form(...),
     preco: float = Form(...),
-    estoque: int = Form(...)
+    estoque: int = Form(...),
+    categoria_id: int = Form(...), # Recebendo o ID do formulário
+    db: Session = Depends(get_db)
 ):
-    return {"nome": nome, "preco": preco, "estoque": estoque}
+    # Cria a instância do modelo
+    novo_produto = Produto(
+        nome=nome, 
+        preco=preco, 
+        estoque=estoque, 
+        categoria_id=categoria_id
+    )
+    
+    # Salva no banco
+    db.add(novo_produto)
+    db.commit()
+    
+    # Redireciona de volta para a home após salvar
+    return RedirectResponse(url="/", status_code=303)
